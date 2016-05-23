add83b7619ceb1d5367a683baa6325443034c0f5
Stephan Bruny <stephan.bruny@live.de>
2016-05-23 14:24:30 +0200

    Create shedrow.js

---
+++ src/shedrow.js
@@ -0,0 +1,48 @@
+(function(root) {
+	if (!root.Pony) {
+		throw new Error("Pony.js Core missing or not loaded");
+	}
+
+	function _createTableHeader(headItem, table) {
+		var th = new root.Pony.el('th', {});
+		if (headItem.head) {
+			th.append(headItem.head(table));
+		} else {
+			th.appendNew('span', {}, headItem.title);
+		}
+		return th;
+	}
+
+	function PonyTable(attributes, head, data) {
+		this.head = head;
+		this.base = new root.Pony.el('table', attributes);
+		this.thead = this.base.appendNew('thead');
+		this.tbody = this.base.appendNew('tbody');
+		this.data = data || [];
+		var headRow = this.thead.appendNew('tr');
+		for (var key in head) {
+			var th = _createTableHeader(head[key], this);
+			headRow.append(th);
+		}
+	}
+
+	PonyTable.prototype.display = function(data, options) {
+		if (!Array.isArray(data)) { throw new Error("Table data must be an array") };
+		this.tbody.clear();
+		for (var i = 0; i < data.length; i++) {
+			var tr = this.tbody.appendNew('tr');
+			for (var key in this.head) {
+				var td = tr.appendNew('td');
+				if (this.head[key].delegate) {
+					td.append(this.head[key].delegate(data[i], options));
+				} else {
+					td.el.appendChild(document.createTextNode((data[i][key])));
+				}
+			}
+		}
+	};
+
+	root.Pony.Shedrow = {
+		Table: PonyTable
+	};
+})(this);
